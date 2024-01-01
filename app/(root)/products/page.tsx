314b53cc7c5c22e4f6f1f5d6e0034c517fa15667
Gunapathi <gunapathi123@gmail.com>
2024-01-02 01:54:16 +0530

    updated json file with live path

--- app/(root)/products/page.tsx
+++ app/(root)/products/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 };
 
 const ProductsPage = async () => {
-	const data = await fetch(`${process.env.STATIC_URL}/api/products`).then(
+	const data = await fetch(`${process.env.DUMMY_JSON_URI}/api/products`).then(
 		(res) => res.json()
 	);
 	return (
--- utils/baseUtils.tsx
+++ utils/baseUtils.tsx
@@ -1,5 +1,5 @@
 export const StaticPath = (url: string) => {
-	const staticPath = process.env.STATIC_URL;
+	const staticPath = process.env.DUMMY_JSON_URI;
     let staticUrl = staticPath + url;
     
 	return staticUrl;
